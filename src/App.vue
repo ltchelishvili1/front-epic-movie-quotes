db285eccd039ea1e66ce2672106127a8e653917e
ltchelishvili1 <levan.tchelishvili@redberry.ge>
2023-05-19 23:13:54 +0400

    feat: clean default project

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
-
 <template>
-<div>
-  <div class="w-[100px] h-[100px] bg-red-100">epic movie quotes</div>
-</div>
+  <div>
+    <div class="w-[100px] h-[100px] bg-red-100">epic movie quotes</div>
+  </div>
 </template>
