10573537dfbfb075da7b39e1a59c65dbc9cdc2a1
ltchelishvili1 <levan.tchelishvili@redberry.ge>
2023-07-10 23:11:27 +0400

    refactor: eliminate modal overflow

--- src/components/modals/SuccessfullyUpdatedModal.vue
+++ src/components/modals/SuccessfullyUpdatedModal.vue
@@ -1,6 +1,6 @@
 <template>
   <section
-    class="absolute bottom-[50%] left-[50%] w-[100vw] h-screen -translate-x-[50%] translate-y-[50%] backdrop-blur-[.2rem] bg-transparent z-50 flex flex-col items-center justify-center"
+    class="absolute bottom-[50%] left-[50%] w-[100vw] h-screen -translate-x-[51%] translate-y-[50%] backdrop-blur-[.2rem] bg-transparent z-50 flex flex-col items-center justify-center"
   >
     <div
       class="bg-[#BADBCC] p-4 flex items-center justify-center rounded scale-[.8] md:scale-[1.3] lg:scale-[1.3]"
